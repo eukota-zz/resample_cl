fe31423d2f205515df5e0bae7d97f469a91ec051
Darrell Ross <eukota@gmail.com>
2017-02-26 16:45:31 -0800

    updated resample test data to use 1024 sample size with inputs and outputs

--- octave/test_data_resample.m
+++ octave/test_data_resample.m
@@ -3,8 +3,9 @@
 % n0*x^0 + n1*x^1 + n2*x^2 + n3*x^3 + n4*x^4 + n5*x^5 + n6*x^6 + n7*x^7
 % 
 
-n=ones(1,8)*2;
-x=1:4;
+n=1:7;
+x=[0:7,8:-1:1];
+x=repmat(x,1,64);
 csvwrite('..\sessions\data\resample_in.csv',x')
-y=n(1)*x.^0 + n(2)*x.^1 + n(3)*x.^2 + n(4)*x.^3 + n(5)*x.^4 + n(6)*x.^5 + n(7)*x.^6 + n(8)*x.^7;
+y=n(1)*x.^0 + n(2)*x.^1 + n(3)*x.^2 + n(4)*x.^3 + n(5)*x.^4 + n(6)*x.^5 + n(7)*x.^6;
 csvwrite('..\sessions\data\resample_out.csv',y')
