5db694d9e1360fc59984e2db2794e2dd344eebfd
Darrell Ross <eukota@gmail.com>
2017-02-28 21:04:40 -0800

    Fixed backSub to work with NxM R matrices.

--- octave/backSub.m
+++ octave/backSub.m
@@ -1,10 +1,10 @@
-% R = Square Matrix of size MxM
+% R = Upper Triangular Matrix of size NxM
 % Qtb = Column Vector of size M
-
+% out = Column Vector of solution to R*out=Qtb
   function [out] = backSub(R, Qtb)
-    s = size(R,1);
-    out = zeros(s,1);
-    for m=s:-1:1
+    [rows,cols] = size(R);
+    out = zeros(cols,1);
+    for m=cols:-1:1
       out(m)=(Qtb(m)-R(m,:)*out)/R(m,m);
     endfor
     
