b55d9c738e0e683da3ec72e80e3d4cba38ae8e60
Darrell Ross <eukota@gmail.com>
2017-03-05 16:36:26 -0800

    Whoops forgot to commit new files.

---
+++ resample/resample/constants.cpp
@@ -0,0 +1,8 @@
+#include "constants.h"
+
+int SAMPLE_SIZE = 1024;
+bool SKIP_VERIFICATION = false;
+bool PRINT_TO_FILE = false;
+const char* RESULTS_FILE = "results.txt";
+unsigned int RUN_COUNT = 1;
+float MAX_DIFF = (float)0.001;
---
+++ resample/resample/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+extern int SAMPLE_SIZE;
+extern bool SKIP_VERIFICATION;
+extern bool PRINT_TO_FILE;
+extern const char* RESULTS_FILE;
+extern unsigned int RUN_COUNT;
+extern float MAX_DIFF;
